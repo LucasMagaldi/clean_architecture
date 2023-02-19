f113dfea727fe0d8c10046e43f1455e429368360
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-02-19 15:29:25 -0300

    updating methods

--- src/Entities/Route/route.spec.ts
+++ src/Entities/Route/route.spec.ts
@@ -1,4 +1,4 @@
-import { RouteTypes, Route } from "./route";
+import { RouteTypes, Route, LatLng } from "./route";
 
 describe('Route Tests', () => {
     test('constructor', () => {
@@ -30,7 +30,7 @@ describe('Route Tests', () => {
         });
     });
 
-    test("Update method ", () => {
+    test("Update Title method ", () => {
         const routeProps = {
             title: "my route",
             startPosition: {lat: 0, lng: 2},
@@ -45,4 +45,57 @@ describe('Route Tests', () => {
         route.updateTitle("Updating");
         expect(route.title).toBe('Updating')
     });
-})
+
+    test("Update position method", () => {
+        const routeProps = {
+            title: "my route",
+            startPosition: {lat: 0, lng: 2},
+            endPosition: {lat: 4, lng: 3},
+        }
+
+        const route = new Route(routeProps);
+        const startPosition: LatLng = {lat: 10, lng: 12};
+        const endPosition: LatLng = {lat: 15, lng: 11};
+        route.updatePosition(startPosition, endPosition);
+        expect(route.startPosition).toBe(startPosition);
+        expect(route.endPosition).toBe(endPosition);
+    });
+
+    test("Update points method", () => {
+        const routeProps = {
+            title: "my route",
+            startPosition: {lat: 0, lng: 2},
+            endPosition: {lat: 4, lng: 3},
+            points: [
+                {lat: 15, lng: -3},
+                {lat: 67, lng: 21}
+            ]
+        }
+
+        const route = new Route(routeProps);
+        const points: LatLng [] = [{lat: 10, lng: 12},{lat: 15, lng: 11}];
+        route.updatePoints(points);
+        expect(route.points).toHaveLength(2)
+        expect(route.points).toBe(points);
+    });
+
+    test("Create Route", () => {
+        const routeProps = {
+            title: "my route",
+            startPosition: {lat: 0, lng: 2},
+            endPosition: {lat: 4, lng: 3}
+        }
+
+        const route = new Route(routeProps);
+        const points: LatLng [] = [
+            {lat: 10, lng: 12},
+            {lat: 15, lng: 11},
+            {lat: 15, lng: -3},
+            {lat: 67, lng: 21},
+            {lat: -8, lng: -3},
+        ];
+        const routes = route.createRoute(points)
+        expect(routes).toBe(points)
+        //expect(route.points).toBe(points);
+    });
+});
--- src/Entities/Route/route.ts
+++ src/Entities/Route/route.ts
@@ -1,4 +1,4 @@
-type LatLng = { lat: number; lng: number };
+export type LatLng = { lat: number; lng: number };
 export type RouteTypes = {
     title: string;
     startPosition: LatLng;
@@ -18,12 +18,52 @@ export class Route {
     updateTitle(title: string) {
         this.title = title;
     }
+    updatePosition(startPosition: LatLng, endPosition: LatLng) {
+        this.updateStartPosition = startPosition;
+        this.updateEndPosition = endPosition;
+    }
+    updatePoints(points: LatLng[]) {
+        this.updatePointsValue = points;
+    }
+
+    createRoute(points: LatLng[]) {
+        let message: string;
+
+        if(points.length < 2)  {
+            message = "Add a valid destination";
+            return message;
+        }
+        if(points.length > 4) {
+            message = "Your trip exceeded stop limit";
+            return message;
+        }
+
+        this.updatePointsValue = points;
+        return this.points;
+    }
 
     get title() {
         return this.props.title;
     }
-
     private set title(value: string) {
         this.props.title = value;
+    }
+    get startPosition() {
+        return this.props.startPosition;
+    }
+    private set updateStartPosition(value: LatLng) {
+        this.props.startPosition = value;
+    }
+    get endPosition() {
+        return this.props.endPosition;
+    }
+    private set updateEndPosition(value: LatLng) {
+        this.props.endPosition = value;
+    }
+    get points() {
+        return this.props.points;
+    }
+    private set updatePointsValue(value: LatLng[]) {
+        this.props.points = value;
     }
 }
